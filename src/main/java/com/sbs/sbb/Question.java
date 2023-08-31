b4a5326eef663ce0ddb7cd2e82a41035d01f47a9
kimaruy <changmin2458@gmail.com>
2023-08-31 09:35:44 +0900

    findBySubject

---
+++ src/main/java/com/sbs/sbb/Question.java
@@ -0,0 +1,32 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity // question 테이블
+public class Question {
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(length = 200) // VARCHAR(200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // mappedBy Answer 클래스의 question 변수 이름을 적어야 합니다.
+    // CascadeType.REMOVE 를 하면 Question 을 삭제할 때 답변도 함께 삭제됩니다.
+    // OneToMany 는 테이블의 칼럼으로 생성되지 않습니다.
+    // 선택
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
